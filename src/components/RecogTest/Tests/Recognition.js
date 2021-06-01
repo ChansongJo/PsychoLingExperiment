1740c12011b18956ac1bfa0b756811835f0d3fe0
Chansong Jo <chansong.jo@navercorp.com>
2021-06-01 10:37:17 +0900

    change yes/no pos

--- src/components/RecogTest/Tests/Recognition.js
+++ src/components/RecogTest/Tests/Recognition.js
@@ -5,7 +5,7 @@ import useEventListener from "@use-it/event-listener";
 // https://github.com/donavon/use-event-listener
 
 const PROGRESS_KEY = [" "];
-const CHOICE_KEY = ["ㄹ", "f", "F", "ArrowLeft", "ㅓ", "j", "J", "ArrowRight"];
+const CHOICE_KEY = ["ㄹ", "f", "F", "ArrowRight", "ㅓ", "j", "J", "ArrowLeft"];
 const CORRECT_KEY = CHOICE_KEY.slice(0, 4)
 const INCORRECT_KEY = CHOICE_KEY.slice(4, 8)
 
@@ -72,17 +72,17 @@ const CommonItem = ({question, img, keyResponse}) => (
             {img && <div className="comment"><Image src={img} style={{height: '300px'}}/></div>}
         </div>
         <div className="choiceSet">
-            <div className="choiceBox" style={{backgroundColor: CORRECT_KEY.includes(keyResponse) ? 'chartreuse' : null }}>
-                <div className='choice'>
-                    예
-                </div>
-                <div className="keyPress">← 키를 눌러 선택하세요</div>
-            </div>
             <div className="choiceBox" style={{ backgroundColor: INCORRECT_KEY.includes(keyResponse) ? 'tomato' : null }}>
                 <div className='choice'>
                     아니오
                 </div>
-                <div className="keyPress">→ 키를 눌러 선택하세요</div>
+                <div className="keyPress">← 키를 눌러 선택하세요</div>
+            </div>
+            <div className="choiceBox" style={{backgroundColor: CORRECT_KEY.includes(keyResponse) ? 'chartreuse' : null }}>
+                <div className='choice'>
+                    예
+                </div>
+                <div className="keyPress">←→ 키를 눌러 선택하세요</div>
             </div>
         </div>
         <div className='instruction'>
